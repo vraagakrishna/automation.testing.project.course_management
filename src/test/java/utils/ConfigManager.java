d4fc29b776ef302dd3cc50991a202bcdcc5ce116
Raaga Krishna Velthapu <vraagakrishna@gmail.com>
2026-03-29 22:22:40 +0200

    Fixed ConfigManger getAppPath

--- src/test/java/utils/ConfigManager.java
+++ src/test/java/utils/ConfigManager.java
@@ -2,6 +2,8 @@ package utils;
 
 import common.Constants;
 
+import java.io.File;
+
 public class ConfigManager {
 
     public static String getAppiumServerUrl() {
@@ -21,7 +23,8 @@ public class ConfigManager {
     }
 
     public static String getAppPath() {
-        return System.getProperty("APP_PATH", "target/app/app-qa-release.apk");
+        return System.getProperty(
+                "APP_PATH", "target" + File.separator + "app" + File.separator + "app-qa-release.apk");
     }
 
     public static String getAppUrl() {
