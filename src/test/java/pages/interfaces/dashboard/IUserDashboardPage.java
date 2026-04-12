c33b705feccd5adaf2091bd0c85103bae619ca58
Raaga Krishna Velthapu <vraagakrishna@gmail.com>
2026-04-12 20:19:08 +0200

    User Enrolled Tests for Web

--- src/test/java/pages/interfaces/dashboard/IUserDashboardPage.java
+++ src/test/java/pages/interfaces/dashboard/IUserDashboardPage.java
@@ -1,7 +1,11 @@
 package pages.interfaces.dashboard;
 
+import models.Course;
+
 public interface IUserDashboardPage {
 
     void clickViewAllCourses();
+
+    void validateEnrolledCourse(Course course);
 
 }
